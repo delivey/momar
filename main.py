4f46fc68c1e2ede15a74a9db5636953d043060d8
delivey <deliveydev@gmail.com>
2022-04-21 17:48:07 +0300

    Very basic movie name algo

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import os.path
+
+def is_integer(str):
+    try:
+        int(str)
+        return True
+    except ValueError:
+        return False
+
+# TODO: make more complete
+def is_show(name):
+    name = name.upper()
+    for idx, letter in enumerate(name):
+        if letter == "S":
+            season_string = name[idx:idx+6]
+            digits = len([ch for ch in season_string if ch.isdigit()])
+            if "E" in season_string and digits == 4:
+                return True
+    return False
+
+
+directory = "D:\Torrents"
+extensions = [".mp4", ".mkv"]
+
+for dirpath, dirnames, filenames in os.walk(directory):
+    for filename in [f for f in filenames]:
+        if filename[-4:] in extensions and not is_show(filename):
+            movie_name_parts = filename.split(".")
+            movie_name = ""
+            for idx, i in enumerate(movie_name_parts):
+                if i.isdigit():
+                    movie_name = movie_name_parts[:idx]
+            print(" ".join(movie_name))
